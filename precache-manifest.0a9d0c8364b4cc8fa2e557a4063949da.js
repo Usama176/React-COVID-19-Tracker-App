4ecb20c8d7e9eae82ef6f3967e90d7fc7b671793
Usama176 <usamaarifshah7@gmail.com>
2021-03-28 23:01:31 +0500

    Updates

---
+++ precache-manifest.0a9d0c8364b4cc8fa2e557a4063949da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b74b87a039c997a3dc34bb6ca841d7e",
+    "url": "/Usama176/React-COVID-19-Tracker-App/index.html"
+  },
+  {
+    "revision": "3e1ca2e2a6e4db2fdd4e",
+    "url": "/Usama176/React-COVID-19-Tracker-App/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "c9c0e965615b36c479d9",
+    "url": "/Usama176/React-COVID-19-Tracker-App/static/css/main.fb55c351.chunk.css"
+  },
+  {
+    "revision": "3e1ca2e2a6e4db2fdd4e",
+    "url": "/Usama176/React-COVID-19-Tracker-App/static/js/2.61c7280e.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/Usama176/React-COVID-19-Tracker-App/static/js/2.61c7280e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9c0e965615b36c479d9",
+    "url": "/Usama176/React-COVID-19-Tracker-App/static/js/main.d6c035e9.chunk.js"
+  },
+  {
+    "revision": "dbab2a6f4a8345bc6be5",
+    "url": "/Usama176/React-COVID-19-Tracker-App/static/js/runtime-main.2fbefb27.js"
+  }
+]);
